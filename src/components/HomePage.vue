fb76e62f90e22bdd7397d812723eb20a78994e23
kotik-spb <shadowty@mail.ru>
2020-04-02 23:24:36 +0300

    Slider components changed

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,68 +1,116 @@
 <template>
-  <main>
-    <div id="podcasts" class="podcasts_block">
-      <podcastItem />
-      <podcastItem />
-      <button class="show-more">Показать больше<dropDownArrow class="show-more_button" /></button>
-    </div>
+  <main id="podcasts">
+    <PodcastBlock />
+    <PodcastBlock />
     <div class="semi-columns-block d-flex justify-between">
       <WhatIs id="whatsIsIt" />
       <PopularPodcasts id="popular" />
     </div>
     <BlogBlock id="blog" />
     <AboutUs id="aboutUs" />
-    <OurTeam id="ourTeam" />
+
+    <!-- OUR TEAM SLIDER -->
+
+    <div class="our-team">
+      <h1 class="title-underlined-right">Наша команда</h1>
+      <Hooper ref="carousel" :infiniteScroll="true" :itemsToShow="2">
+        <Slide
+          class="our-team_group d-flex justify-around"
+          v-for="item in teamItems"
+          :key="item.name"
+        >
+          <TeamItem :item="item"></TeamItem>
+        </Slide>
+      </Hooper>
+      <button class="our-team_next-group" @click.prevent="slideNext"></button>
+    </div>
   </main>
 </template>
 
 <script>
-import podcastItem from './base/podcastItem.vue';
-import dropDownArrow from './base/dropDownArrow.vue';
+import { Hooper, Slide } from 'hooper';
+import PodcastBlock from './PodcastBlock.vue';
 import WhatIs from './WhatIs.vue';
 import PopularPodcasts from './PopularPodcasts.vue';
 import BlogBlock from './BlogBlock.vue';
 import AboutUs from './AboutUs.vue';
-import OurTeam from './OurTeam.vue';
+import TeamItem from './base/TeamItem.vue';
 
 export default {
+  data() {
+    return {
+      teamItems: [
+        {
+          photo: 'team-photo1.png',
+          name: 'Екатерина Кронгауз',
+          description:
+            'Работала на телеканале "Дождь", возглавляла издание snob.ru, создала отдел подкастов в "ауч"',
+          contacts: {
+            instagram: '',
+            vk: '',
+          },
+        },
+        {
+          photo: 'team-photo2.png',
+          name: 'Лика Кремер',
+          description:
+            'Работала на телеканале "Дождь", возглавляла издание snob.ru, создала отдел подкастов в "ауч"',
+          contacts: {
+            instagram: '',
+            vk: '',
+          },
+        },
+        {
+          photo: 'team-photo2.png',
+          name: 'Лика Кремер',
+          description:
+            'Работала на телеканале "Дождь", возглавляла издание snob.ru, создала отдел подкастов в "ауч"',
+          contacts: {
+            instagram: '',
+            vk: '',
+          },
+        },
+        {
+          photo: 'team-photo2.png',
+          name: 'Лика Кремер',
+          description:
+            'Работала на телеканале "Дождь", возглавляла издание snob.ru, создала отдел подкастов в "ауч"',
+          contacts: {
+            instagram: '',
+            vk: '',
+          },
+        },
+      ],
+    };
+  },
   name: 'Home',
   components: {
-    podcastItem,
-    dropDownArrow,
+    PodcastBlock,
     WhatIs,
     PopularPodcasts,
     BlogBlock,
     AboutUs,
-    OurTeam,
+    Hooper,
+    Slide,
+    TeamItem,
+  },
+  methods: {
+    slideNext() {
+      this.$refs.carousel.slideNext();
+    },
   },
 };
 </script>
 
 <style lang="scss">
-.podcasts_block {
-  margin-bottom: 10%;
-}
-
-.show-more {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 auto;
-  font-size: $font-size-text;
-  &_button {
-    margin-top: 10%;
-    transform: scale(1.25);
-  }
-}
-
 .semi-columns-block {
+  margin: 10% 0;
   @media screen and (max-width: $mq-mob) {
     flex-direction: column;
   }
 }
 
 .semi-columns-block > * {
-  margin-bottom: 10%;
   &:nth-child(1) {
     width: 40%;
   }
@@ -76,6 +124,65 @@ export default {
     &:nth-child(2) {
       width: 100%;
     }
+  }
+}
+
+.our-team {
+  position: relative;
+
+  &_next-group {
+    margin: 1%;
+    position: absolute;
+    top: 50%;
+    right: 3%;
+    &::after,
+    &::before {
+      content: "";
+      background-color: $colour-accent;
+      height: 3px;
+      width: 20px;
+      position: absolute;
+    }
+    &::before {
+      transform: rotate(-55deg);
+      top: 7px;
+    }
+    &::after {
+      transform: rotate(55deg);
+      top: -7px;
+    }
+  }
+
+  &_item {
+    margin: 0 4%;
+  }
+
+  &_member-photo {
+    align-self: baseline;
+    width: 11vw;
+  }
+
+  &_member-info {
+    width: 60%;
+    margin-left: 4%;
+  }
+
+  &_member-name {
+    font-weight: bold;
+    font-size: 2.25rem;
+    margin-bottom: 2%;
+  }
+
+  &_member-contacts-item {
+    width: 11%;
+    margin: 4% 4% 0 0;
+    height: auto;
+  }
+
+  &_member-charge {
+    font-family: $font-family-Lora;
+    font-size: $font-size-text;
+    line-height: 2.5rem;
   }
 }
 </style>
---
+++ src/components/base/TeamItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="our-team_item d-flex">
+    <img class="our-team_member-photo" :src="require(`../../assets/${item.photo}`)" alt="photo" />
+    <div class="our-team_member-info d-flex flex-column">
+      <span class="our-team_member-name">{{ item.name }}</span>
+      <p class="our-team_member-charge">{{ item.description }}</p>
+      <div class="our-team_member-contacts">
+        <iconFacebook class="our-team_member-contacts-item" />
+        <iconInstagram class="our-team_member-contacts-item" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import iconFacebook from '../icons/iconFacebook.vue';
+import iconInstagram from '../icons/iconInstagram.vue';
+
+export default {
+  name: 'TeamItem',
+  props: ['item'],
+  components: {
+    iconFacebook,
+    iconInstagram,
+  },
+};
+</script>
